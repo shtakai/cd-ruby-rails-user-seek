682c5104d2080ac017177111da3a2e2901832324
Alyson Takai <shtakai@gmail.com>
2016-06-15 22:49:22 -0700

    implemented users#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/users/#{@user.id}"
+    else
+      flash[:errors] = @user.errors.messages
+      redirect_to '/users/new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'sessions' => 'sessions#create'
   get 'users/new' => 'users#new'
   get 'users/:id' => 'users#show'
+  post 'users' => 'users#create'
   delete 'sessions' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
