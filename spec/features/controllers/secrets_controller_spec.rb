52e24d70086d91eb7ababb4f6e26a1e5be081b75
Alyson Takai <shtakai@gmail.com>
2016-06-16 16:54:27 -0700

    created spec

---
+++ spec/features/controllers/secrets_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe SecretsController, type: :controller do
+  before do
+    @user = create_user
+    @secret = @user.secrets.create content: 'secret'
+  end
+
+  describe 'when not logged in' do
+    before do
+      session[:user_id] = nil
+    end
+
+    it 'cannot access index' do
+      get :index
+      expect(response).to redirect_to '/sessions/new'
+    end
+
+    it 'cannot access create' do
+      post :create
+      expect(response).to redirect_to '/sessions/new'
+    end
+
+    it 'cannot access destroy' do
+      delete :destroy, id: @secret
+      expect(response).to redirect_to '/sessions/new'
+    end
+  end
+end
