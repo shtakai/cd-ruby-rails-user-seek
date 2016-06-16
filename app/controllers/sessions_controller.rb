3bcc021d3dbebdfb0ecd29128173ec1b1f640ec9
Alyson Takai <shtakai@gmail.com>
2016-06-15 21:05:00 -0700

    renamed session[:user_id]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:user][:email])
     if @user.authenticate(params[:user][:password])
-      session[:user] = @user.id
+      session[:user_id] = @user.id
       redirect_to "/users/#{@user.id}"
     else
       session.clear
