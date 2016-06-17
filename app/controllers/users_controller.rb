66217f67a46ead8dbe2b4de3212009df6b07c168
Alyson Takai <shtakai@gmail.com>
2016-06-16 20:18:25 -0700

    modified for non-authorized user (User after registration)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to "/users/#{@user.id}"
+      flash[:info] = 'Registered your account, please login.'
+      redirect_to "/sessions/new"
     else
       flash[:errors] = @user.errors.messages
       redirect_to '/users/new'
--- spec/features/users/create_user_spec.rb
+++ spec/features/users/create_user_spec.rb
@@ -11,8 +11,7 @@ RSpec.describe 'creating a user' do
     fill_in 'Password', with: 'password'
     fill_in 'Password Confirmation', with: 'password'
     click_button 'Join'
-    last_user = User.last
-    expect(current_path).to eq "/users/#{last_user.id}"
+    expect(current_path).to eq "/sessions/new"
   end
 
   it 'shows validation errors without proper credentials' do
